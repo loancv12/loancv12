4b7015c5f38e344dd673365774858e32f3b6093f
loancv12 <loancv12@gmail.com>
2022-08-08 22:12:22 +0700

    Initial comment

---
+++ main.js
@@ -0,0 +1,11 @@
+// const $ = document.queryselector.bind(document)
+let likeElement = document.querySelector(".home-product-item__like");
+likeElement.onclick = function(){
+   if(this.classList.contains("home-product-item__like--liked")){
+      this.classList.remove("home-product-item__like--liked") 
+   }
+   else {
+       this.classList.add("home-product-item__like--liked") 
+       console.log(likeElement)
+   }
+}
